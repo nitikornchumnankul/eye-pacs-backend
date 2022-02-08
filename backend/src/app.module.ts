59bc66d597d38fd98cb1d260caf512c5d6435936
Rayato159 <rayato159@gmail.com>
2022-02-08 23:28:26 +0700

    :sparkles: database design in progress.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -20,7 +20,7 @@ import { UsersModule } from './users/users.module';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
         return {
-          type: 'mysql',
+          type: 'postgres',
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
           username: configService.get('DB_USERNAME'),
--- frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-import loginReducer from '../features/loginSlice'
-
-export default configureStore({
-  reducer: {
-      login: loginReducer,
-  }
-})
